efa9a479add0e4692888085abae3252a812f3418
gabriellgon <38075308+gabriellgon@users.noreply.github.com>
2018-06-27 01:39:12 -0300

    Update p6.c

--- p6.c
+++ p6.c
@@ -1,16 +1,27 @@
-//Gabriel Carneiro Gonçalves
+//Gabriel Carneiro GonÃ§alves
 //11611ECP013
 
 #include <stdio.h>
 
+int divide(int somar, int quanti)
+{
+	float media;
+	
+	media=(float)somar/(float)quanti;
+	printf("\nA media dos numeros informados e: %.2f",media);
+	
+	return 0;
+}
+
 void soma ( )
 {
-	int vet[20]={0}, quant, i=0, j=0, somar=0;
+	int vet[20]={0}, quant, i, j, somar=0;
 	
 	printf("Digite quantos numeros deseja informar: ");
 	scanf("%i",&quant);
 	getchar( );
 	printf("\nDigite os numeros que deseja informar: ");
+
 	for(i=0;i<quant;i++)
 	{
 		scanf("%i",&vet[i]);
@@ -21,17 +32,8 @@ void soma ( )
 	{
 		somar=somar+vet[j];
 	}
+	
 	divide(somar,quant);
-}
-
-int divide(int somar, int quanti)
-{
-	float media;
-	
-	media=(float)somar/(float)quanti;
-	printf("\nA media dos numeros informados e: %.2f",media);
-	
-	return 0;
 }
 
 int main ( )
